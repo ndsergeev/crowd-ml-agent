c7a340537dca5ee65441c29e3fe6afe5861095b1
sergeevkep@icloud.com <163R17ck25081973p>
2020-03-23 21:02:02 +1100

    Initial commit

---
+++ Assets/Scripts/CrowdAgent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using MLAgents;
+
+public class CrowdAgent : Agent
+{
+    public GameObject env;
+
+    public override void Initialize()
+    {
+
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+
+    }
+
+    public override void OnEpisodeBegin()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        
+    }
+
+}
